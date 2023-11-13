6175765e0ed5621ba9b62216ecdac8b67aefc7a7
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2023-11-13 08:08:58 +0600

    feat: created generator_api app.

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -11,7 +11,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -19,6 +19,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+LOCAL_APPS = [
+    "generator_api",
+]
+
+INSTALLED_APPS = DJANGO_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
